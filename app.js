0596fff5a1ae445a3713c50bdee8b7ff17805291
Eatngofr <146079107+Eatngofr@users.noreply.github.com>
2023-10-22 18:09:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const openai = require('openai')
-const config = require('./config')
 
 dotenv.config()
 
 const app = express()
 
-app.post('/chat', async (req, res) => {
+app.post('/chat', (req, res) => {
   const message = req.body.message
 
   const response = await openai.completions.create({
     engine: 'davinci-codex',
     prompt: message,
-    max_tokens: config.maxTokens,
+    max_tokens: 100,
   })
 
   res.send(response.choices[0].text)
